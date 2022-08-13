e643522ae176ddfe7f5e23384d122349d34ba55f
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-08-13 12:47:13 -0700

    Prep for session 3

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import json
 app = Flask(__name__)
 
 # DB init and config
-db_filename = "session1.db"
+db_filename = "te_one.db"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
@@ -20,34 +20,25 @@ def greet():
     return "Hello"
 
 
-@app.route('/developers/', methods=["POST"])
-def create_developer():
-
-    body =  json.loads(request.data)
-    n, l = body.get("nickname"), body.get("lang")
-
-    dev = Developer(nickname=n, lang=l)
-    db.session.add(dev)
-    db.session.commit()
-
-    return "Created"
-
 @app.route('/developers/', methods=['GET'])
 def get_developers():
-
+    """ Get all developers """
     res = [dev.serialize() for dev in Developer.query.all()] 
     return json.dumps(res)
 
 
+@app.route('/developers/', methods=["POST"])
+def create_developer():
+    """ Create a developer """
 
+    body =  json.loads(request.data)
+    n, l = body.get("nickname"), body.get("language")
 
+    dev = Developer(nickname=n, language=l)
+    db.session.add(dev)
+    db.session.commit()
 
-    
-
-
-
-
-
+    return "Created"
 
 
 
--- db.py
+++ db.py
@@ -2,17 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
 class Developer(db.Model):
-    __tablename__ = 'developers'
+    __tablename__ = 'developer'
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String)
-    lang = db.Column(db.String)
+    language = db.Column(db.String)
 
 
     def serialize(self):
         return ({
             "id" : self.id,
             "name" : self.nickname,
-            "lang" : self.lang
+            "lang" : self.language
         })
